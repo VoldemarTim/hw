420e3da2b51b8e1ae47b3d263bc0e4ed753d97a9
VoldemarTim <tim4enco@mail.ru>
2021-05-18 22:52:58 +0300

    сделал дз

--- 4.py
+++ 4.py
@@ -5,3 +5,13 @@
 # ```
 #
 # Подсказка: использовать возможности python, изученные на уроке.
+
+
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+for i in range(len(src) - 1):
+    n = [src[i]]
+    i += 1
+    result = [src[i]]
+    if result > n:
+        n = result
+        print(result, end='')
