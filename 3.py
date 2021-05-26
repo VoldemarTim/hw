90c8428aca65f8264252c421ce4c7d5777385e18
VoldemarTim <tim4enco@mail.ru>
2021-05-26 23:36:07 +0300

    сделал дз

--- 3.py
+++ 3.py
@@ -1,3 +1,18 @@
-3. * (вместо 2) Доработать функцию currency_rates(): теперь она должна возвращать кроме курса дату,
-которая передаётся в ответе сервера. Дата должна быть в виде объекта date.
-Подумайте, как извлечь дату из ответа, какой тип данных лучше использовать в ответе функции?
+# 3. * (вместо 2) Доработать функцию currency_rates(): теперь она должна возвращать кроме курса дату,
+# которая передаётся в ответе сервера. Дата должна быть в виде объекта date.
+# Подумайте, как извлечь дату из ответа, какой тип данных лучше использовать в ответе функции?
+
+import requests
+from datetime import datetime
+from decimal import Decimal
+
+
+def currency_rates(val):
+    response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+    date = datetime.strptime(response.headers['Date'], '%a, %d %B %Y %H:%M:%S %Z').date()
+    response = response.json()
+    if not response['Valute'].get(val.upper()):
+        return
+    return f'{Decimal(str(response["Valute"][val.upper()]["Value"]))}, {date}'
+
+print(currency_rates('USD'))
