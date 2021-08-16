34f46a6b45494b80f7b356028c5d6e7088e5d073
VoldemarTim <tim4enco@mail.ru>
2021-08-16 22:23:14 +0300

    сделал дз

---
+++ dz_2.py
@@ -0,0 +1,41 @@
+def _parser_hh(vacancy):
+    vacancy_date = []
+
+    params = {
+        'text': vacancy, \
+        'search_field': 'name', \
+        'items_on_page': '100', \
+        'page': ''
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
+    }
+
+    link = 'https://hh.ru/search/vacancy'
+
+    html = requests.get(link, params=params, headers=headers)
+
+    if html.ok:
+        parsed_html = bs(html.text, 'html.parser')
+
+        page_block = parsed_html.find('div', {'data-qa': 'pager-block'})
+        if not page_block:
+            last_page = '1'
+        else:
+            last_page = int(page_block.find_all('a', {'class': 'HH-Pager-Control'})[-2].getText())
+
+    for page in range(0, last_page):
+        params['page'] = page
+        html = requests.get(link, params=params, headers=headers)
+
+        if html.ok:
+            parsed_html = bs(html.text, 'html.parser')
+
+            vacancy_items = parsed_html.find('div', {'data-qa': 'vacancy-serp__results'}) \
+                .find_all('div', {'class': 'vacancy-serp-item'})
+
+            for item in vacancy_items:
+                vacancy_date.append(_parser_item_hh(item))
+
+    return vacancy_date
